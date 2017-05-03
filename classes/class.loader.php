d41813dcefaa0179c1ac54b93c237b6d0eabc3eb
Lasse Lentz Thomsen <kontakt@lasselentz.dk>
2017-05-03 18:57:06 +0200

    Update structure of classes

--- classes/class.loader.php
+++ classes/class.loader.php
@@ -3,5 +3,46 @@ if(!defined('WPINC')) die;
 
 class Loader
 {
+	protected $actions;
+	protected $filters;
 
+	public function __construct()
+	{
+		$this->actions = [];
+		$this->filters = [];
+	}
+
+	public function addAction($hook, $component, $callback)
+	{
+		$this->actions = $this->add($this->actions, $hook, $component, $callback);
+	}
+
+	public function addFilter($hook, $component, $callback)
+	{
+		$this->filters = $this->add($this->filters, $hook, $component, $callback);
+	}
+
+	public function init()
+	{
+		foreach($this->filters as $hook)
+		{
+			add_filter($hook['hook'], [$hook['component'], $hook['callback']]);
+		}
+
+		foreach($this->actions as $hook)
+		{
+			add_filter($hook['hook'], [$hook['component'], $hook['callback']]);
+		}
+	}
+
+	private function add($hooks, $hook, $component, $callback)
+	{
+		$hooks[] = [
+			'hook'      => $hook,
+			'component' => $component,
+			'callback'  => $callback,
+		];
+
+		return $hooks;
+	}
 }
--- classes/class.slideshow.php
+++ classes/class.slideshow.php
@@ -3,8 +3,36 @@ if(!defined('WPINC')) die;
 
 class Slideshow
 {
+	protected $loader;
+	protected $plugin_slug;
+	protected $version;
+
+	public function __construct()
+	{
+		$this->plugin_slug = 'Slideshow';
+		$this->version     = '0.1.0';
+
+		$this->loadDependencies();
+	}
+
+	public function addResources()
+	{
+		// Styles and scripts goes here
+	}
+
+	private function loadDependencies()
+	{
+		require_once plugin_dir_path(__FILE__) . 'class.loader.php';
+		$this->loader = new Loader();
+	}
+
 	public function init()
 	{
-		
+		$this->loader->init();
+	}
+
+	public function getVersion()
+	{
+		return $this->version;
 	}
 }
